7d8ae366eb64c4892fc574f152d4bb504dd2f5d3
PickingUpPieces <v.picking@gmx.de>
2021-01-30 20:07:36 +0100

    Add spacebar

--- scripts/new_space-window.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-yabai -m space --create && index="$(yabai -m query --spaces --display | jq 'map(select(."native-fullscreen" == 0))[-1].index')" && yabai -m window --space "${index}" && yabai -m space --focus "${index}"
