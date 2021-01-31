2b48da3bf5fa3c14bc6d4aa9e203f3eb55dde178
Vincent Picking <vincent.picking@tum.de>
2021-01-31 13:11:35 +0100

    Tidy up scripts and comment them; Add firefox open shortcut

--- scripts/empty_space2.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-yabai -m query --spaces --display | \
-     jq -re 'map(select(."native-fullscreen" == 0)) | length > 1' \
-     && yabai -m query --spaces | \
-          jq -re 'map(select(."windows" == [] and ."focused" == 0).index) | reverse | .[] ' | \
-          xargs -I % sh -c 'yabai -m space % --destroy'
-
