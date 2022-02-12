c7314b1bfcf0fdc50b04887d2ea7b8ed8cf349ad
PickingUpPieces <v.picking@gmx.de>
2022-02-12 18:25:45 +0100

    Fix creating spaces; Destroy spaces when only minimized windows are on them

--- scripts/destroy_empty_space.sh
+++ scripts/destroy_empty_space.sh
@@ -1,12 +1,36 @@
 #!/usr/bin/env bash
 
-#TODO Check if on the space only a scratchpad window id is left AND minimized (or maybe in general a minimized window). If that's the case move this window to current space or space 1.
+spaces=$(yabai -m query --spaces | jq -c '.[]')
+composable_spaces=()
+
+# Check windows on every space if they are minimized
+for space in $spaces
+do
+    window_ids=$(echo $space | jq -re '.windows | .[]')
+    minimized_windows=0
+    for window in $window_ids
+    do
+        minimized_windows=$(($minimized_windows + $(yabai -m query --windows --window "$window" | jq -re 'if ."is-minimized" then 1 else 0 end'))) 
+    done
+
+    if [[ "$minimized_windows" -eq "${#window_ids[@]}" ]] && [[ $(echo $space | jq 'if ."is-native-fullscreen" then 1 else 0 end') -eq 0 ]]; then
+        echo "Destroy Space ID " $(echo $space | jq '.index') 
+        composable_spaces+=( $(echo $space | jq '.index') )
+    fi
+done
+
+# Destroy spaces in reversed order
+composable_spaces=$(echo $composable_spaces | rev)
+for space in $composable_spaces
+do
+    yabai -m space "$space" --destroy
+done
+
 #TODO Check for focus; Bug when using two displays with one empty desktop, that desktop gets destroyed
 
 # Check if on current display is now a fullscreen window --> Then the previous space where window was before is empty, if it was the only window. Don't delete this space then.
 yabai -m query --spaces --display | \
     # Check if a space is in native-fullscreen
-<<<<<<< HEAD
     jq -re 'map(select(."is-native-fullscreen" == false)) | length > 1' \
     && yabai -m query --spaces | \
     # Check for a space with no windows AND isn't focused at the moment
