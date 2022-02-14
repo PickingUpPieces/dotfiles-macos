dc95f93ee48fc7ae947d2965a6019003257fc4c7
PickingUpPieces <v.picking@gmx.de>
2022-02-14 14:12:15 +0100

    Cleanup scratchpad script

--- scripts/scratchpad.sh
+++ scripts/scratchpad.sh
@@ -3,9 +3,8 @@
 # Generic Scratchpad script
 # $1 (First parameter): Program name
 
-scratchpad_id=$(yabai -m query --windows | jq --arg program "$1" '.[] | select(.app==$program).id')
+scratchpad_id=$(yabai -m query --windows | jq --arg app "$1" '.[] | select(.app==$app).id')
 
-# TODO: Safe window id of current focused window before opening the scratchpad. Set this ID as env variable. This can be used to focus the correct window again when two or more scratchpad windows have been openend.
 
 if [[ "$scratchpad_id" -lt 1 ]]; then
   scratchpad_id=$(open -a "$1" | awk '{print $NF}')
@@ -21,8 +20,6 @@ else
     yabai -m window --focus "$scratchpad_id"
     yabai -m window --grid 6:6:1:1:4:4
   else
-      # TODO Check if current focused window isn't minimized and is on the current space
-      # TODO If not, focus current space; Deals with the problem of double opened scratchpad windows
     yabai -m window "$scratchpad_id" --minimize
     window_id=$(yabai -m query --windows --space | jq 'map(select(."is-minimized" == false and ."is-hidden" == false)) | .[0].id')
     yabai -m window --focus $window_id
