e3bbc971000a64e81fe8af9c211446b07c1ae2af
Vincent Picking <v.picking@gmx.de>
2022-02-19 12:11:08 +0100

    Fix scratchpad rocketChat; Add open_firefox script

--- scripts/scratchpad_RocketChat.sh
+++ scripts/scratchpad_RocketChat.sh
@@ -3,7 +3,7 @@
 # Scratchpad script for Rocket.Chat because application name (Rocket.Chat.app) isn't running app name (RocketChat) - just great
 # $1 (First parameter): Program name
 
-scratchpad_id=$(yabai -m query --windows | jq --arg program "$1" '.[] | select(.app==$program).id')
+scratchpad_id=$(yabai -m query --windows | jq --arg app "$1" '.[] | select(.app==$app).id')
 echo $scratchpad_id
 
 if [[ "$scratchpad_id" -lt 1 ]]; then
@@ -12,13 +12,13 @@ if [[ "$scratchpad_id" -lt 1 ]]; then
   yabai -m window --focus "$scratchpad_id"
   yabai -m window --grid 6:6:1:1:4:4
 else
-  is_minimized=$(yabai -m query --windows --window "$scratchpad_id" | jq '.minimized')
-  current_space=$(yabai -m query --spaces --space | jq '.index')
+  is_minimized=$(yabai -m query --windows --window "$scratchpad_id" | jq '."is-minimized"')
+  current_space=$(yabai -m query --spaces --space | jq '."index"')
 
   echo $is_minimized
   echo $current_space
 
-  if [[ "$is_minimized" -eq 1 ]]; then
+  if [[ "$is_minimized" = "true" ]]; then
     yabai -m window "$scratchpad_id" --space "$current_space"
     yabai -m window --focus "$scratchpad_id"
     yabai -m window --grid 6:6:1:1:4:4
