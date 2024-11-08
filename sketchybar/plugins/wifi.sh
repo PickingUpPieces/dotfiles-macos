5243f205baeed4240147351283949d2d5eeae1eb
Vincent Picking <v.picking@gmx.de>
2024-11-08 21:51:30 +0100

    Update wifi bar for macos 18

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -3,8 +3,8 @@
 source "$HOME/.config/sketchybar/colors.sh"
 source "$HOME/.config/sketchybar/icons.sh"
 
-CURRENT_WIFI="$(networksetup -getairportnetwork en0)"
-SSID="$(echo "$CURRENT_WIFI" | grep "Current Wi-Fi Network" | sed 's/^Current Wi-Fi Network: //')"
+#CURRENT_WIFI="$(networksetup -getairportnetwork en0)"
+SSID="$(ipconfig getsummary en0 | awk -F ' SSID : '  '/ SSID : / {print $2}')"
 IP_ADDRESS=$(scutil --nwi | grep address | sed 's/.*://' | tr -d ' ' | head -1)
 IS_VPN=$(scutil --nwi | grep -m1 'utun' | awk '{ print $1 }')
 
